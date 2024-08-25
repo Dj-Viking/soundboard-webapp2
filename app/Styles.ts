a3ea35608688badad1b8aad194ae58c0a2937acf
dj-viking <anders.swedishviking@gmail.com>
2024-08-25 15:55:59 -0400

    format

--- app/Styles.ts
+++ app/Styles.ts
@@ -3,86 +3,86 @@ export function createStyles(): HTMLStyleElement {
     const style = document.createElement("style");
     style.innerHTML = `
         html {
-                background-color: grey;
-            }
+            background-color: grey;
+        }
 
-            .control-device-display-container {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                height: auto;
-                width: auto;
-                border-radius: 10px;
-                margin-bottom: 10px;
-                border: solid green 3px;
-            }
-        
-            .control-svg-container {
-                height: auto;
-                width: auto;
-                display: flex;
-                flex-direction: row;
-                justify-content: space-around;
-                align-items: center;
-            }
+        .control-device-display-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            height: auto;
+            width: auto;
+            border-radius: 10px;
+            margin-bottom: 10px;
+            border: solid green 3px;
+        }
+    
+        .control-svg-container {
+            height: auto;
+            width: auto;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: center;
+        }
 
-            .soundboard-container {
-                display: flex;
-                flex-wrap: wrap;
-                margin: 0 auto;
-                height: ${SOUNDBOARD_DIM}px;
-                width: ${SOUNDBOARD_DIM}px;
-                background-color: white;
-                border-radius: 10px;
-            }
+        .soundboard-container {
+            display: flex;
+            flex-wrap: wrap;
+            margin: 0 auto;
+            height: ${SOUNDBOARD_DIM}px;
+            width: ${SOUNDBOARD_DIM}px;
+            background-color: white;
+            border-radius: 10px;
+        }
 
-            .volume-container {
-                display: flex;
-                margin: 0 auto;
-                height: auto;
-                width: auto;
-                flex-direction: column;
-                align-items: center;
-                margin-bottom: 10px;
-            }
+        .volume-container {
+            display: flex;
+            margin: 0 auto;
+            height: auto;
+            width: auto;
+            flex-direction: column;
+            align-items: center;
+            margin-bottom: 10px;
+        }
 
-            .header {
-                color: blue;
-                text-align: center;
-            }
+        .header {
+            color: blue;
+            text-align: center;
+        }
 
-            .track-progress {
-                width: 70%;
-            }
+        .track-progress {
+            width: 70%;
+        }
 
-            .midi-selector-container {
-                width: 70%;
-                margin: 0 auto;
-            }
+        .midi-selector-container {
+            width: 70%;
+            margin: 0 auto;
+        }
 
-            .midi-selector {
-                width: 100%;
-            }
+        .midi-selector {
+            width: 100%;
+        }
 
-            .btn-control-container {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-            }
+        .btn-control-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
 
-            .board-button {
-                color: white;
-                height: ${ADD_BUTTON_DIM}px;
-                width: auto;
-                background-color: blue;
-                margin-bottom: 20px;
-                border-radius: 10px;
-            }
+        .board-button {
+            color: white;
+            height: ${ADD_BUTTON_DIM}px;
+            width: auto;
+            background-color: blue;
+            margin-bottom: 20px;
+            border-radius: 10px;
+        }
 
-            .soundboard-button {
-                height: ${BUTTON_DIM}px;
-                width: ${BUTTON_DIM}px;
-            }
+        .soundboard-button {
+            height: ${BUTTON_DIM}px;
+            width: ${BUTTON_DIM}px;
+        }
     `;
     return style;
 }
